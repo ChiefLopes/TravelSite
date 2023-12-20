0d57d01775aad902f1881aa142f6bce48cab627d
Louis Peace <chieflopesekede@gmail.com>
2023-12-20 15:06:53 +0100

    implemented the destination page with its styles

--- src/Components/Destinations/Destinations.jsx
+++ src/Components/Destinations/Destinations.jsx
@@ -1,10 +1,12 @@
-import "./Destination.scss"
+import "./Destination.scss";
 import { MdLocationPin } from "react-icons/md";
-
+import { BsFillCreditCardFill } from "react-icons/bs";
+import { BsFillCalendar2DateFill } from "react-icons/bs";
+import { BiSearchAlt } from "react-icons/bi";
 
 const Destinations = () => {
   return (
-    <div className="destination section cont">
+    <div className="destination section container">
       <div className="secContainer">
         <div className="">
           <span className="redText">EXPLORE NOW</span>
@@ -15,11 +17,45 @@ const Destinations = () => {
           </p>
         </div>
 
-        <div className="searchFiled grid">
+        <div className="searchField grid">
           <div className="inputField flex">
-            <MdLocationPin className="icon"/>
+            <MdLocationPin className="icon" />
+            <input type="text" placeholder="Location" />
           </div>
+
+          <div className="inputField flex">
+            <BsFillCreditCardFill className="icon" />
+            <input type="text" placeholder="Budget" />
+          </div>
+
+          <div className="inputField flex">
+            <BsFillCalendar2DateFill className="icon" />
+            <input type="text" placeholder="Date" />
+          </div>
+
+          <button className="btn flex">
+            <BiSearchAlt className="icon" /> Search
+          </button>
         </div>
+
+        <div className="secMenu">
+          <ul className="flex">
+                      <li className="active">All</li>
+                      <li>Recommended</li>
+                      <li>Beach</li>
+                      <li>Park</li>
+                      <li>Nature</li>
+                      <li>Mountain</li>
+          </ul>
+              </div>
+              
+              <div className="destinationContainer grid">
+                  <div className="singleDestination">
+                      <div className="imgDiv">
+                           
+                      </div>
+                  </div>
+              </div>
       </div>
     </div>
   );
