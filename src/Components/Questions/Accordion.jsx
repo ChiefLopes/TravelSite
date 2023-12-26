3258a5df5fdfc4b1e9122881e2eabddd11374b69
Louis Peace <chieflopesekede@gmail.com>
2023-12-26 06:11:53 +0100

    styling the accordion and question components using the question.scss file

--- src/Components/Questions/Accordion.jsx
+++ src/Components/Questions/Accordion.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import "./Questions.scss"
+import { BsArrowDownCircleFill } from "react-icons/bs";
 
 const Accordion = () => {
   return (
-    <div>Accordion</div>
-  )
-}
+    <div className="accordionContainer">
+      <span className="title flex">
+        How do I choose the right destination for me?
+        <span>
+          <BsArrowDownCircleFill className="icon" />
+        </span>
+      </span> 
+      <p>Consider your interests, budget desired experiences, and the type of environment you enjoy. Research destinations that align with your preferences and offer attractions or activities you find appealing.</p>
+    </div>
+  );
+};
 
-export default Accordion
+export default Accordion;
