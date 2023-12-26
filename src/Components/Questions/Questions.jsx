c81406d676cb7afa3607ec8f1d0fd7aeef606dc0
Louis Peace <chieflopesekede@gmail.com>
2023-12-26 23:36:40 +0100

    Adding functionalities to the icon on the accordion

--- src/Components/Questions/Questions.jsx
+++ src/Components/Questions/Questions.jsx
@@ -4,7 +4,7 @@ import "./Questions.scss";
 
 const Questions = () => {
   const [active, setActive] = useState(
-    "title2"
+    "How do I choose the right travel destinations for me?"
   );
 
   return (
@@ -16,14 +16,20 @@ const Questions = () => {
       <div className="sceContainer grid">
         <div className="accordion grid">
           <Accordion
-            title="title1"
+            title="How do I choose the right travel destinations for me?"
             desc="Consider your interests, budget desired experiences, and the type of environment you enjoy. Research destinations that align with your preferences and offer attractions or activities you find appealing."
             active={active}
             setActive={setActive}
           />
           <Accordion
-            title="title2"
-            desc="Consider your interests, budget desired experiences, and the type of environment you enjoy. Research destinations that align with your preferences and offer attractions or activities you find appealing."
+            title="What are the best times to visit specific destinations?"
+            desc="Research the climate, weather patterns and peak tourist seasons of the destination you are interested in. Opt for the shoulder seasons when the weather is pleasant, and crowds are fewer if possible"
+            active={active}
+            setActive={setActive}
+          />
+          <Accordion
+            title="What are the best times to visit specific destinations?"
+            desc="Research the climate, weather patterns and peak tourist seasons of the destination you are interested in. Opt for the shoulder seasons when the weather is pleasant, and crowds are fewer if possible"
             active={active}
             setActive={setActive}
           />
