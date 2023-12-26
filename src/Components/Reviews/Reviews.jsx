f85a8b2eca53469e16174b7f88112d9d233bf2ab
Louis Peace <chieflopesekede@gmail.com>
2023-12-26 04:46:54 +0100

    added styles and stars to the review page

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { AiFillStar } from "react-icons/ai";
 
 const Reviews = () => {
   return (
-    <div>Reviews</div>
-  )
+    <div className="review section container">
+      <div className="secContainer grid">
+        <div className="textDiv">
+          <span className="redText">FROM OUR CLIENTS</span>
+          <h3>Real Travel History From Our Beloved Clients</h3>
+          <p>
+            by choosing us as their tour agency, customers can expect an
+            enriching an enriching and enjoyable travel experience, filled with
+            unforgettable memories that will last a lifetime.
+          </p>
+          <span className="stars flex">
+            <AiFillStar className='icon'/>
+            <AiFillStar className='icon'/>
+            <AiFillStar className='icon'/>
+            <AiFillStar className='icon'/>
+            <AiFillStar className='icon'/>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Reviews
