a94250748249d26063c10fd42a058a3e0370e9e6
Louis Peace <chieflopesekede@gmail.com>
2023-12-26 05:51:25 +0100

    still working on the questions page

--- src/Components/Questions/Questions.jsx
+++ src/Components/Questions/Questions.jsx
@@ -2,8 +2,30 @@ import "./Questions.scss"
 
 const Questions = () => {
   return (
-    <div>Questions</div>
-  )
+    <div className="questions section container">
+      <div className="secHeading">
+        <h3>Frequently Asked Questions</h3>
+      </div>
+
+      <div className="sceContainer grid">
+        <div className="accordion grid"></div>
+
+        <div className="form">
+          <div className="secHeading">
+                      <h4>Do You Have any specific questions?</h4>
+                      <p>
+                          Please fill the form below and our dedicated team will get in touch with you as soon as possible.
+                      </p>
+                  </div>
+                  
+                  <div className="formContent grid">
+                      <input type="email" placeholder="Enter e-mail address" />
+                      <textarea placeholder="Enter Your question here"></textarea>
+                  </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Questions
