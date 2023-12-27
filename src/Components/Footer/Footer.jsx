cbe3f71e7e1aad5c1be02528d866915a4b5f7153
Louis Peace <chieflopesekede@gmail.com>
2023-12-27 08:11:03 +0100

    added links to the footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,15 +8,39 @@ const Footer = () => {
     <div className="footer">
       <div className="secContainer container grid">
         <div className="logoDiv">
-                  <div className="footerLogo">
-                      <BiLogoMediumOld className="icon" />
-                      <span>Success-Trips</span>
+          <div className="footerLogo">
+            <BiLogoMediumOld className="icon" />
+            <span>Success-Trips</span>
+          </div>
+
+          <div className="socials ">
+            <ImFacebook2 className="icon" />
+            <FaTwitter className="icon" />
+            <FaInstagramSquare className="icon" />
                   </div>
                   
-                  <div className="socials flex">
-                      <ImFacebook2 />
-                      <FaTwitter />
-                      <FaInstagramSquare />
+                  <div className="footerLinks">
+                      <span className="span linkTitle">
+                          Information
+                      </span>
+                      <li>
+                          <a href="#">Home</a>
+                          <a href="#">Explore</a>
+                          <a href="#">Travel</a>
+                          <a href="#">Blog</a>
+                      </li>
+                  </div>
+                  
+                  <div className="footerLinks">
+                      <span className="span linkTitle">
+                          Helpful Links
+                      </span>
+                      <li>
+                          <a href="#">Destination</a>
+                          <a href="#">Support</a>
+                          <a href="#">Travel and condition</a>
+                          <a href="#">Privacy</a>
+                      </li>
                   </div>
         </div>
       </div>
