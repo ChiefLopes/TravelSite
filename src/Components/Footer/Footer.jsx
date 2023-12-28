a0300ab7d219aaf893ee6dd45214348f246e7b7e
Louis Peace <chieflopesekede@gmail.com>
2023-12-28 20:32:01 +0100

    added more styles to the footer link

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import "./Footer.scss"
+import "./Footer.scss";
 
 // Imported social icons
 import { BiLogoMediumOld } from "react-icons/bi";
@@ -15,53 +15,48 @@ const Footer = () => {
             <BiLogoMediumOld className="icon" />
             <span>Success-Trips</span>
           </div>
+        <div className="socials ">
+          <ImFacebook2 className="icon" />
+          <FaTwitter className="icon" />
+          <FaInstagramSquare className="icon" />
+        </div>
+        </div>
 
-          <div className="socials ">
-            <ImFacebook2 className="icon" />
-            <FaTwitter className="icon" />
-            <FaInstagramSquare className="icon" />
-                  </div>
-                  
-                  <div className="footerLinks">
-                      <span className="span linkTitle">
-                          Information
-                      </span>
-                      <li>
-                          <a href="#">Home</a>
-                      </li>
-                      <li>
-                          <a href="#">Explore</a>
-                      </li>
-                      <li>
-                          <a href="#">Travel</a>
-                      </li>
-                      <li>
-                          <a href="#">Blog</a>
-                      </li>
-                  </div>
-                  
-                  <div className="footerLinks">
-                      <span className="linkTitle">
-                          Helpful Links
-                      </span>
-                      <li>
-                          <a href="#">Destination</a>
-                      </li>
-                      <li>
-                          <a href="#">Support</a>
-                      </li>
-                      <li>
-                          <a href="#">Travel and Condition</a>
-                      </li>
-                      <li>
-                          <a href="#">Privacy</a>
-                      </li>
-                  </div>
-                  <div className="footerLinks">
-                      <span className="linkTitle">Contact Details</span>
-                      <span className="phone">+234-990-224-567-0</span>
-                      <span className="email">chieflopesekede@gmail.com</span>
-                  </div>
+        <div className="footerLinks">
+          <span className="span linkTitle">Information</span>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Explore</a>
+          </li>
+          <li>
+            <a href="#">Travel</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+        </div>
+
+        <div className="footerLinks">
+          <span className="linkTitle">Helpful Links</span>
+          <li>
+            <a href="#">Destination</a>
+          </li>
+          <li>
+            <a href="#">Support</a>
+          </li>
+          <li>
+            <a href="#">Travel and Condition</a>
+          </li>
+          <li>
+            <a href="#">Privacy</a>
+          </li>
+        </div>
+        <div className="footerLinks">
+          <span className="linkTitle">Contact Details</span>
+          <span className="phone">+234-990-224-567-0</span>
+          <span className="email">chieflopesekede@gmail.com</span>
         </div>
       </div>
     </div>
