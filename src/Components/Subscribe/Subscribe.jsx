fdc7be49775ee2dc9b4d2185d23836a8be46c94e
Louis Peace <chieflopesekede@gmail.com>
2024-01-03 14:04:31 +0100

    added the scroll animation into the subscribe page

--- src/Components/Subscribe/Subscribe.jsx
+++ src/Components/Subscribe/Subscribe.jsx
@@ -1,14 +1,23 @@
 import photo1 from "../../Asset/clients/client1.jpg";
-import "./Subscribe.scss"
+import "./Subscribe.scss";
 
+import { useEffect } from "react";
+
+// Imported Library for scroll animation
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Subscribe = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  });
+
   return (
     <div className="subscribe section container">
       <div className="secContainer grid">
-        <img src={photo1} alt="Div Image" />
+        <img src={photo1} alt="Div Image" data-aos="fade-left" />
 
-        <div className="textDiv">
+        <div className="textDiv" data-aos="fade-right">
           <h4>Best way to Start Your Journey!</h4>
           <p>
             We offer personalized itineraries tailored to individual preferences
