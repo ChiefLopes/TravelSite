8cc9aa05f94e308e3a721de85c7099a74850c253
Louis Peace <chieflopesekede@gmail.com>
2024-01-08 23:33:46 +0100

    retouch on the media query

--- src/Layout/Scroll.jsx
+++ src/Layout/Scroll.jsx
@@ -6,6 +6,7 @@ import React, { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
+// Creating a context API for the scroll animation
 const Scroll = ({ children }) => {
   useEffect(() => {
     Aos.init({ duration: 2000 });
